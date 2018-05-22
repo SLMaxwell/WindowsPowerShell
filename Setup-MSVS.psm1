648bd0a7b6efeabafa958d8b1f88630f4675f2b5
Scott L. Maxwell <scott.maxwell@rammount.com>
2018-05-22 13:44:49 -0400

    Update with blank file containing Microsoft Visual Studio Environment variables.

---
+++ Setup-MSVS.psm1
@@ -0,0 +1,49 @@
+#############################################
+#
+#    Set the needed environment variables
+#    For Microsoft Visual Studio
+#    (so that powershell runs as if it is
+#     in the VS development environment)
+#
+#############################################
+
+# $env:CommandPromptType="Native"
+# $env:DevEnvDir="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\"
+# $env:ExtensionSdkDir="C:\Program Files (x86)\Microsoft SDKs\Windows Kits\10\ExtensionSDKs"
+# $env:Framework40Version="v4.0"
+# $env:FrameworkDir="C:\Windows\Microsoft.NET\Framework\"
+# $env:FrameworkDir32="C:\Windows\Microsoft.NET\Framework\"
+# $env:FrameworkVersion="v4.0.30319"
+# $env:FrameworkVersion32="v4.0.30319"
+
+# $env:UCRTVersion="10.0.16299.0"
+# $env:UniversalCRTSdkDir="C:\Program Files (x86)\Windows Kits\10\"
+
+# $env:VCIDEInstallDir="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\VC\"
+# $env:VCINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\"
+# $env:VCToolsInstallDir="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.11.25503\"
+# $env:VCToolsRedistDir="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Redist\MSVC\14.11.25325\"
+# $env:VisualStudioVersion="15.0"
+# $env:VS150COMNTOOLS="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\"
+# $env:VSCMD_ARG_app_plat="Desktop"
+# $env:VSCMD_ARG_HOST_ARCH="x86"
+# $env:VSCMD_ARG_TGT_ARCH="x86"
+# $env:VSCMD_VER="15.0"
+# $env:VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\"
+
+# $env:WindowsLibPath="C:\Program Files (x86)\Windows Kits\10\UnionMetadata\10.0.16299.0;C:\Program Files (x86)\Windows Kits\10\References\10.0.16299.0"
+# $env:WindowsSdkBinPath="C:\Program Files (x86)\Windows Kits\10\bin\"
+# $env:WindowsSdkDir="C:\Program Files (x86)\Windows Kits\10\"
+# $env:WindowsSDKLibVersion="10.0.16299.0\"
+# $env:WindowsSdkVerBinPath="C:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\"
+# $env:WindowsSDKVersion="10.0.16299.0\"
+# $env:__DOTNET_ADD_32BIT="1"
+# $env:__DOTNET_PREFERRED_BITNESS="32"
+
+# $env:INCLUDE="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.11.25503\include;C:\Program Files (x86)\Windows Kits\10\include\10.0.16299.0\ucrt;C:\Program Files (x86)\Windows Kits\10\include\10.0.16299.0\shared;C:\Program Files (x86)\Windows Kits\10\include\10.0.16299.0\um;C:\Program Files (x86)\Windows Kits\10\include\10.0.16299.0\winrt;"
+# $env:LIB="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.11.25503\lib\x86;C:\Program Files (x86)\Windows Kits\10\lib\10.0.16299.0\ucrt\x86;C:\Program Files (x86)\Windows Kits\10\lib\10.0.16299.0\um\x86;"
+# $env:LIBPATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.11.25503\lib\x86;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.11.25503\lib\x86\store\references;C:\Program Files (x86)\Windows Kits\10\UnionMetadata\10.0.16299.0;C:\Program Files (x86)\Windows Kits\10\References\10.0.16299.0;C:\Windows\Microsoft.NET\Framework\v4.0.30319;"
+
+# $env:__VSCMD_PREINIT_PATH= $env:path
+# $prefixPaths = "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.11.25503\bin\HostX86\x86;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\VC\VCPackages;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\bin\Roslyn;C:\Program Files (x86)\Windows Kits\10\bin\x86;C:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x86;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\\MSBuild\15.0\bin;C:\Windows\Microsoft.NET\Framework\v4.0.30319;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\;"
+# $env:path = "$prefixPaths$env:path"
